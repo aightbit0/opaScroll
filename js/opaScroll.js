ca158c3333afeaa83c40d85ca5efee80aa64030d
aightbit0 <svenlatka@web.de>
2020-12-05 14:30:48 +0100

    new method jquery scroll function

--- js/opaScroll.js
+++ js/opaScroll.js
@@ -1,5 +1,5 @@
 //set time to wait after scroll
-var time = 250
+var time = 500
 
 //set the height to scroll
 var height = window.innerHeight
@@ -8,45 +8,56 @@ var finish_up = false;
 var finish_down = false;
 var timer;
 
+var actualState = 0;
+
+var ids = [];
+
 document.addEventListener('scroll', function(e) {
 	if(timer !== null) {
         clearTimeout(timer);        
     }
     timer = setTimeout(function() {
+		console.log("beendet");
 		 finish_up = false
 		 finish_down = false
     }, time);
 });
 
 document.addEventListener("wheel", function(e){
+	console.log(actualState);
 	e.preventDefault();
+	if (actualState == ids.length-1) {
+		console.log("am ende");
+		finish_up = false
+		finish_down = false
+	}
 	if (e.deltaY < 0)
 	{
 		if(finish_down == false){
 			finish_down = true;
-			scrollup()
+			if(actualState != 0){
+				actualState -=1;
+				scrolldown(ids[actualState]);
+			}
 		}
 	}
 	else if (e.deltaY > 0){
 		if(finish_up == false){
 			finish_up = true;
-			scrolldown();
+			if(actualState < ids.length){
+			actualState +=1;
+			scrolldown(ids[actualState]);
+			}
 		}
 	}
 }, {passive: false} );
 	
-function scrolldown(){
-	window.scrollBy({
-	top: height,
-	left: 0,
-	behavior: 'smooth'
-	});
-}
-
-function scrollup(){
-	window.scrollBy({
-	top: -height,
-	left: 0,
-	behavior: 'smooth'
-	});
+function scrolldown(toid){
+	if(actualState >= ids.length){
+		return
+	}else{
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $(toid).offset().top
+		}, 300);
+	}
 }
