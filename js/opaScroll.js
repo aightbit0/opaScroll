06849ba55135f722c0534a295d8e7246ebeae340
aightbit0 <svenlatka@web.de>
2020-12-06 17:38:40 +0100

    code fixup and cleanup

--- js/opaScroll.js
+++ js/opaScroll.js
@@ -1,63 +1,87 @@
 //set time to wait after scroll
-var time = 500
+var time = 200;
 
 //set the height to scroll
-var height = window.innerHeight
+var height = 0;
+
+//ids to jump to
+var ids = [];
+
+//set animation duration
+animationTime = 600;
 
 var finish_up = false;
 var finish_down = false;
 var timer;
 
 var actualState = 0;
+var endpage = false;
 
-var ids = [];
-
-document.addEventListener('scroll', function(e) {
+$(window).scroll(function() {
 	if(timer !== null) {
         clearTimeout(timer);        
     }
     timer = setTimeout(function() {
-		console.log("beendet");
+		console.log("ende")
 		 finish_up = false
 		 finish_down = false
     }, time);
-});
+	if($(window).scrollTop() + $(window).height() >= $(document).height()-30) {
+		endpage = true;
+	}
+ });
 
 document.addEventListener("wheel", function(e){
-	console.log(actualState);
 	e.preventDefault();
-	if (actualState == ids.length-1) {
-		console.log("am ende");
-		finish_up = false
-		finish_down = false
-	}
 	if (e.deltaY < 0)
 	{
+		endpage = false;
 		if(finish_down == false){
 			finish_down = true;
-			if(actualState != 0){
-				actualState -=1;
-				scrolldown(ids[actualState]);
-			}
+			scrolldown("up");
 		}
 	}
 	else if (e.deltaY > 0){
 		if(finish_up == false){
 			finish_up = true;
-			if(actualState < ids.length){
-			actualState +=1;
-			scrolldown(ids[actualState]);
-			}
+			scrolldown("down");
 		}
 	}
 }, {passive: false} );
 	
-function scrolldown(toid){
-	if(actualState >= ids.length){
-		return
-	}else{
+function scrolldown(direction){
+	if(ids.length != 0){
+		if(actualState >= ids.length){
+			return
+		}
+	}
+	if(ids.length >0 && height==0){
+		if(direction == "up"){
+			if(actualState != 0){
+				actualState -=1;
+			}
+		}else{
+			if(actualState+1 < ids.length){
+				actualState +=1;
+			}
+		}
 		$([document.documentElement, document.body]).animate({
-			scrollTop: $(toid).offset().top
-		}, 300);
+			scrollTop: $(ids[actualState]).offset().top
+		}, animationTime);
+	}else if(ids.length == 0 && height > 0){
+		if(direction == "up"){
+			if(actualState != 0){
+				actualState -=1;
+			}
+		}else{
+			if(endpage == false){
+				actualState +=1;
+			}
+		}
+		$([document.documentElement, document.body]).animate({
+			scrollTop: height*actualState
+		}, animationTime);
+	}else{
+		console.error("invalid configuration opaScroll.js");
 	}
 }
